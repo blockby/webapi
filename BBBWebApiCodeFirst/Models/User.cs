8d5d6fd1efe5903620a9f7b03c5c9b1a30390749
Mario Andres Ravina <ayarmarxio@gmail.com>
2019-05-09 12:03:17 +0200

    lastcommit

---
+++ BBBWebApiCodeFirst/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBBWebApiCodeFirst.Models
+{
+    public class User
+    {
+        [Key]
+        public long id_user { get; set; }
+
+        public string name { get; set; }
+
+        public int id_type_user {get; set;}
+
+        public int depent { get; set; }
+
+        public string description { get; set; }
+
+
+
+        
+    }
+}
