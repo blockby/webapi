587a461cc23ee7e048f4a5425fba9fec53c790c7
Mario Andres Ravina <ayarmarxio@gmail.com>
2019-08-26 14:00:24 +0200

    local commit for small changes

--- BBBWebApiCodeFirst/Models/User.cs
+++ BBBWebApiCodeFirst/Models/User.cs
@@ -10,16 +10,6 @@ namespace BBBWebApiCodeFirst.Models
 {
     public class User: IdentityUser<int>
     {
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        //public int id_user { get; set; }
-
-        //public string name { get; set; }
-
-        //public int id_user_type { get; set; }
-        //[ForeignKey("id_user_type")]
-        //public User_type user_type { get; set; }
-
         public int depend { get; set; }
 
         public string description { get; set; }
